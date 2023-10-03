6962097d87424ce8fb2900ca7ea8359df4109099
Belkawwwwwww <alnblkrv@gmail.com>
2023-10-03 20:05:41 +0500

    login,register

--- src/components/RegisterForm/Register.tsx
+++ src/components/RegisterForm/Register.tsx
@@ -1,95 +1,94 @@
-import React, {FC, useState} from 'react';
+import React, { ChangeEvent, FC, useState } from "react";
 import styles from "../LoginForm/Auth.module.sass";
-import {useAppDispatch, useAppSelector} from "../../store/hooks/redux";
-import {errorUserSelector} from "../../store/slices/UserSlice";
-import {register} from "../../store/action/userAction";
-
+import { useAppDispatch, useAppSelector } from "../../store/hooks/redux";
+import { errorUserSelector } from "../../store/slices/UserSlice";
+import { register } from "../../store/action/userAction";
 
 const Registration: FC = () => {
+  const dispatch = useAppDispatch();
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+  const error = useAppSelector(errorUserSelector);
 
-    const dispatch = useAppDispatch()
-    const [username, setUsername] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const [passwordConfirm, setPasswordConfirm] = useState<string>('')
-    const error = useAppSelector(errorUserSelector)
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (username && password && passwordConfirm)
+      dispatch(register(username, password, passwordConfirm));
+  };
 
-
-    const submit = (e: any) => {
-        e.preventDefault();
-        if (username && password && passwordConfirm) dispatch(register(username, password, passwordConfirm))
+  const onHandlerUser = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "username") {
+      setUsername(e.target.value);
     }
+    if (e.target.name === "password") {
+      setPassword(e.target.value);
+    }
+    if (e.target.name === "passwordConfirm") {
+      setPasswordConfirm(e.target.value);
+    }
+  };
 
-
-    return (
-        <div className={styles.content}>
-            <div className={styles.form}>
-                <form
-                    action=""
-                    className={styles.btnBox}
-                    onSubmit={submit}
-                >
-                    <h1 className={styles.title}>Registration</h1>
-                    {error && <div style={{color: 'red', margin: '10px'}}>
-                        {error}
-                    </div>}
-                    <div className={styles.inputBox}>
-                        <label className={styles.icon} htmlFor="username">
-                            <img src="/img/icon-user.svg" alt="user"/>
-                        </label>
-                        <input
-                            value={username}
-                            onChange={e => setUsername(e.target.value)}
-                            type="text"
-                            name="username"
-                            id="username"
-                            placeholder="Username"
-                            required
-                        />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <label htmlFor="password" className={styles.icon}>
-                            <img src="/img/icon-password.svg" alt="password"/>
-                        </label>
-                        <input
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                            type="password"
-                            name="password"
-                            id="psw-1"
-                            placeholder="Password"
-                            required
-                        />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <label htmlFor="password" className={styles.icon}>
-                            <img src="/img/icon-password.svg" alt="password"/>
-                        </label>
-                        <input
-                            value={passwordConfirm}
-                            onChange={e => setPasswordConfirm(e.target.value)}
-                            type="password"
-                            name="passwordConfirm"
-                            id="psw-2"
-                            placeholder="Confirm Password"
-                            required
-                        />
-                    </div>
-                    <div className={styles.btnBox}>
-                        <button type="submit">Register</button>
-                    </div>
-                </form>
-                <div className={styles.subtitle}>
-                    <a
-                        className={styles.link}
-                        href={'/login'}
-                    >
-                        Log In
-                    </a>
-                </div>
-            </div>
-
+  return (
+    <div className={styles.content}>
+      <div className={styles.form}>
+        <form action="" className={styles.btnBox} onSubmit={handleSubmit}>
+          <h1 className={styles.title}>Registration</h1>
+          {error && <div style={{ color: "red", margin: "10px" }}>{error}</div>}
+          <div className={styles.inputBox}>
+            <label className={styles.icon} htmlFor="username">
+              <img src="/img/icon-user.svg" alt="user" />
+            </label>
+            <input
+              value={username}
+              onChange={onHandlerUser}
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div className={styles.inputBox}>
+            <label htmlFor="password" className={styles.icon}>
+              <img src="/img/icon-password.svg" alt="password" />
+            </label>
+            <input
+              value={password}
+              onChange={onHandlerUser}
+              type="password"
+              name="password"
+              id="psw-1"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className={styles.inputBox}>
+            <label htmlFor="password" className={styles.icon}>
+              <img src="/img/icon-password.svg" alt="password" />
+            </label>
+            <input
+              value={passwordConfirm}
+              onChange={onHandlerUser}
+              type="password"
+              name="passwordConfirm"
+              id="psw-2"
+              placeholder="Confirm Password"
+              required
+            />
+          </div>
+          <div className={styles.btnBox}>
+            <button type="submit">Register</button>
+          </div>
+        </form>
+        <div className={styles.subtitle}>
+          <a className={styles.link} href={"/login"}>
+            Log In
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
--- src/store/action/userAction.ts
+++ src/store/action/userAction.ts
@@ -1,53 +1,46 @@
-import {AppDispatch} from "../index";
-import {userSlice} from "../slices/UserSlice";
+import { AppDispatch } from "../index";
+import { userSlice } from "../slices/UserSlice";
 import axios from "axios";
-import {IUser} from "../../models/models";
 
-export const login = (username: string, password: string) => async (dispatch: AppDispatch) => {
+export const login =
+  (username: string, password: string) => async (dispatch: AppDispatch) => {
     try {
-        const response = await axios.get<IUser[]>(`http://localhost:3000/users/${username}`)
-        const data = await response
-        if (data) {
-            localStorage.setItem('auth', 'true');
-            dispatch(userSlice.actions.setAuth(true))
-        } 
-
+      const response = await axios.get<{ id: string; password: string }>(
+        `http://localhost:3000/users/${username}`,
+      );
+      if (response.status !== 200) {
+        dispatch(userSlice.actions.setError("Некорректный логин"));
+      } else if (response.data.password !== password) {
+        dispatch(userSlice.actions.setError("Некорректный пароль"));
+      } else {
+        localStorage.setItem("auth", "true");
+        dispatch(userSlice.actions.setAuth(true));
+      }
     } catch (e) {
-        dispatch(userSlice.actions.setError('Некорректный логин или пароль'))
+      dispatch(userSlice.actions.setError("Некорректный логин или пароль"));
     }
-
-
-    // localStorage.setItem('username', username)
-    // localStorage.setItem('auth', 'true');
-    // dispatch(userSlice.actions.setAuth(true))
-    // axios, fetch
-    //const response = await axios.get<IUser[]>(`http://localhost:3000/users/${username}`)
-    // обработать запрос
-    // сверить пароль -- если ок авторизовать -- нет - не авторизовать
-    // const mockUsers = response.data.find(user => user.password === password)
-    // if (mockUsers) {
-    //     dispatch(userSlice.actions.setAuth(true))
-    //     localStorage.setItem('auth', 'true');
-    // } else {
-    //     dispatch(userSlice.actions.setError('Некорректный логин или пароль'))
-    // }
-
-}
+  };
 
 export const logout = () => async (dispatch: AppDispatch) => {
-    dispatch(userSlice.actions.setAuth(false))
-    dispatch(userSlice.actions.setError(undefined));
-    localStorage.removeItem('auth')
-    localStorage.removeItem('username')
-}
-export const register = (username: string, password: string, passwordConfirm: string) => async (dispatch: AppDispatch) => {
+  dispatch(userSlice.actions.setAuth(false));
+  dispatch(userSlice.actions.setError(undefined));
+  localStorage.removeItem("auth");
+  localStorage.removeItem("username");
+};
+export const register =
+  (username: string, password: string, passwordConfirm: string) =>
+  async (dispatch: AppDispatch) => {
     try {
-        dispatch(userSlice.actions.setLoading(true))
-        const newUser = await axios.get(`http://localhost:3000/users/`)
-        console.log(newUser);
-        dispatch(userSlice.actions.setLoading(false))
+      const response = await axios.get<{
+        id: string;
+        password: string;
+        passwordConfirm: string;
+      }>(`http://localhost:3000/users/${username}`);
+      if (response.status === 200) {
+        dispatch(userSlice.actions.setError("Такой логин уже существует"));
+      } else {
+      }
     } catch (e) {
-        dispatch(userSlice.actions.setError('Произошла ошибка при регистрации'))
-
+      dispatch(userSlice.actions.setError("Произошла ошибка при регистрации"));
     }
-}
+  };
