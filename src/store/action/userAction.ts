a342141a2d6544f887efd8b1c06c2f54d2cabd37
Belkawwwwwww <alnblkrv@gmail.com>
2023-10-25 13:40:27 +0500

    action - checkAuth, 404NotFound, App

--- src/store/action/userAction.ts
+++ src/store/action/userAction.ts
@@ -2,7 +2,6 @@ import { AppDispatch } from "../index";
 import { userSlice } from "../slices/UserSlice";
 // import axios from "axios";
 import ax from "../../utils/axios";
-import { IUser } from "../../models/models";
 
 export const checkAuth = (user_id: number) => async (dispatch: AppDispatch) => {
   try {
@@ -10,18 +9,16 @@ export const checkAuth = (user_id: number) => async (dispatch: AppDispatch) => {
       answercode: number;
       answer: string;
     }>(`/profile?user_id=${user_id}`);
-      console.log(response);
-      if (response.data.answercode === 1) {
-        dispatch(userSlice.actions.setAuth(true));
+    console.log(response);
+    if (response.data.answercode === 1) {
+      dispatch(userSlice.actions.setAuth(true));
     } else if (response.data.answercode === 2) {
       dispatch(userSlice.actions.setError(response.data.answer));
-      localStorage.removeItem("user_id")
     } else if (response.data.answercode === 2) {
       dispatch(userSlice.actions.setError(response.data.answer));
-      localStorage.removeItem("user_id")
     }
   } catch (e) {
-      dispatch(userSlice.actions.setError("Произошла ошибка"));
+    dispatch(userSlice.actions.setError("Произошла ошибка"));
   }
 };
 export const login =
@@ -35,7 +32,6 @@ export const login =
       console.log(response);
         if (response.data.answercode === 1) {
             dispatch(userSlice.actions.setAuth(true));
-            dispatch(userSlice.actions.setUser(response.data.data.user_id));
             dispatch(userSlice.actions.setError(undefined));
             localStorage.setItem("user_id", response.data.data.user_id);
         } else if (response.data.answercode === 3) {
@@ -50,7 +46,6 @@ export const logout = () => async (dispatch: AppDispatch) => {
     localStorage.removeItem("user_id");
     dispatch(userSlice.actions.setAuth(false));
     dispatch(userSlice.actions.setError(undefined));
-    dispatch(userSlice.actions.setUser({} as IUser));
 };
 export const register =
   (username: string, password: string, passwordConfig: string) =>
